0f52cddec8fc38e21b3e64209b61bf163ca4c039
ercsn <33rcsn@gmail.com>
2025-07-05 21:20:59 -0300

    go

---
+++ src/main/java/com/btg_pactual/btg_pactual_backend/BtgPactualBackendApplication.java
@@ -0,0 +1,13 @@
+package com.btg_pactual.btg_pactual_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgPactualBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtgPactualBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btg_pactual/btg_pactual_backend/BtgPactualBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg_pactual.btg_pactual_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 				BtgPactualBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
