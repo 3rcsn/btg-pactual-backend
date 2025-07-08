b373e84faeb1a03c1ae2e7c7c1ed501ffe26902b
ercsn <33rcsn@gmail.com>
2025-07-08 00:23:09 -0300

    kafka

--- src/main/java/com/btg_pactual/btg_pactual_backend/BtgPactualBackendApplication.java
+++ src/main/java/com/btg_pactual/btg_pactual_backend/BtgPactualBackendApplication.java
@@ -1,13 +1,30 @@
 package com.btg_pactual.btg_pactual_backend;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class BtgPactualBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BtgPactualBackendApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("bpg-pactual-backend")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+
+	@KafkaListener(id = "bpg-pactual-backend", topics="bpg-pactual-backend")
+	public  void listen(String in) {
+		System.out.println(in);
 	}
 
 }
