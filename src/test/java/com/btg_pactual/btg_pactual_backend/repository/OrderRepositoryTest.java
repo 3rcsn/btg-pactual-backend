ac753e1f04a332acfc6893f863c2032e84228e40
ercsn <33rcsn@gmail.com>
2025-10-17 09:57:16 -0300

    tests start

---
+++ src/test/java/com/btg_pactual/btg_pactual_backend/repository/OrderRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.btg_pactual.btg_pactual_backend.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataMongoTest
+class OrderRepositoryTest {
+
+    @Test
+    void findAllByCustomerId() {
+    }
+}
---
+++ src/test/java/com/btg_pactual/btg_pactual_backend/service/OrderServiceTest.java
@@ -0,0 +1,20 @@
+package com.btg_pactual.btg_pactual_backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAllByCustomerId() {
+    }
+
+    @Test
+    void findTotalOnOrdersByCustomerId() {
+    }
+}
